132f2fef4b0e41a1929f3fde6667b46386dad0c5
prafullKrRj <prafullkumar384@gmail.com>
2023-11-05 20:01:19 +0530

    total

--- app/src/main/java/com/example/crypto/ui/screens/listScreens/ListScreen.kt
+++ app/src/main/java/com/example/crypto/ui/screens/listScreens/ListScreen.kt
@@ -1,14 +1,19 @@
 package com.example.crypto.ui.screens.listScreens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.crypto.Screens
@@ -82,10 +87,19 @@ fun CoinsList(coins: List<CryptoDetail>, navController: NavHostController) {
         list.add(coin)
     }
     LazyColumn {
+        item {
+            Row (Modifier.fillMaxWidth()){
+                Text(
+                    text = "Total: ${coins.size}",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 22.sp
+                )
+            }
+        }
         items(coins.size) { idx ->
             (list[idx]).also {
                 ListItemComposable(
-                    modifier = Modifier.padding(vertical = 4.dp),
+                    modifier = Modifier.padding(vertical = 8.dp),
                     coin = it,
                     idx = idx + 1,
                     navController = navController
